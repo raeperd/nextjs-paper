21f332f59723fd2da70ea74e4d1d037b3bf66d0f
raeperd <raeperd117@gmail.com>
2022-01-08 07:30:01 +0900

    #1 Add menus in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,7 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-type HeaderProps = {
-  siteName: string
-}
-
-export default function Header({ siteName }: HeaderProps) {
+export default function Header({ siteName, menus }: HeaderProps) {
   return (
     <header className="header">
       <h1 className="logo">
@@ -14,6 +11,30 @@ export default function Header({ siteName }: HeaderProps) {
         {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
         <a className="btn-dark" />
       </h1>
+      <MenuNav menus={menus} />
     </header>
+  )
+}
+
+type HeaderProps = {
+  siteName: string,
+  menus: Menu[]
+}
+
+type Menu = {
+  name: string,
+  href: string
+}
+
+function MenuNav({ menus }: {menus: Menu[]}) {
+  const router = useRouter()
+  return (
+    <nav className="menu">
+      {menus.map((menu) => (
+        <Link href={menu.href}>
+          <a className={router.pathname === menu.href ? 'active' : ''}>{menu.name}</a>
+        </Link>
+      ))}
+    </nav>
   )
 }
