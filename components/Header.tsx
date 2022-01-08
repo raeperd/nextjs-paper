684effc6deb265f99b0cb1a0b4a98ae3719539bc
raeperd <41039751+raeperd@users.noreply.github.com>
2022-01-08 23:30:37 +0900

    #6 Implements Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,65 @@
-import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-export default function Header() {
+export default function Header({ siteName, menus, socials }: HeaderProps) {
   return (
-    <Head>
-      <title> nextjs-paper </title>
-    </Head>
+    <header className="header">
+      <h1 className="logo">
+        <Link href="/">
+          <a className="site-name">{siteName}</a>
+        </Link>
+        {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+        <a className="btn-dark" />
+      </h1>
+      <MenuNav menus={menus} />
+      <SocialNav socials={socials} />
+    </header>
   )
+}
+
+export interface HeaderProps {
+  siteName: string,
+  menus: MenuProps[],
+  socials: SocialProps[]
+}
+
+function MenuNav({ menus }: {menus: MenuProps[]}) {
+  const router = useRouter()
+  return (
+    <nav className="menu">
+      {menus.map((menu) => (
+        <Link href={menu.href}>
+          <a className={router.pathname === menu.href ? 'active' : ''}>{menu.name}</a>
+        </Link>
+      ))}
+    </nav>
+  )
+}
+
+interface MenuProps {
+  name: string,
+  href: string
+}
+
+function SocialNav({ socials }: {socials: SocialProps[]}) {
+  return (
+    <nav className="social">
+      {socials
+        .filter((social) => social.id)
+        .map((social) => (
+          <a href={`//${social.name}.com/${social.id}`}>
+            <img
+              id={social.name}
+              src={`./${social.name}.svg`}
+              alt={`${social.name}`}
+            />
+          </a>
+        ))}
+    </nav>
+  )
+}
+
+interface SocialProps {
+  name: 'instagram' | 'github' | 'twitter',
+  id?: string
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
-import Header from '../components/Header';
+import Header, { HeaderProps } from '../components/Header';
 
-function Index() {
+export default function Index({ siteName, menus, socials }: HeaderProps) {
   return (
-    <Header />
+    <Header siteName={siteName} menus={menus} socials={socials} />
   )
 }
 
-export default Index
+export async function getStaticProps(): Promise<{ props: HeaderProps }> {
+  const siteName = process.env.SITE_NAME ? process.env.SITE_NAME : 'Paper'
+  return {
+    props: {
+      siteName,
+      menus: [{ name: 'About', href: '/about' }],
+      socials: [
+        { name: 'github', id: process.env.GITHUB },
+        { name: 'twitter', id: process.env.TWITTER },
+        { name: 'instagram', id: process.env.INSTAGRAM },
+      ],
+    },
+  }
+}
