fd2089b51f12bbb395c1640eb4594cc449066443
raeperd <raeperd117@gmail.com>
2022-01-09 23:03:28 +0900

    #8 Implements ArticlePreview component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
 import Head from 'next/head';
+import { Article, getAllArticles } from '../lib/article';
 
-export default function Index({ siteName }: {siteName: string}) {
+export default function Index({ siteName, posts }: IndexProps) {
   return (
-    <Head>
-      <title>{siteName}</title>
-    </Head>
+    <>
+      <Head>
+        <title>{siteName}</title>
+      </Head>
+      {posts.map((post) => <ArticlePreview article={post} />)}
+    </>
   )
 }
 
-export async function getStaticProps() {
+function ArticlePreview({ article }: {article: Article}) {
+  return (
+    // TODO: ADD LINK
+    <article className="post-entry" key={article.slug}>
+      <h2>{article.title}</h2>
+      <time>{article.date}</time>
+    </article>
+  )
+}
+
+type IndexProps = {
+  siteName: string,
+  posts: Article[]
+}
+
+export async function getStaticProps(): Promise<{props: IndexProps}> {
   const siteName = process.env.SITE_NAME ? process.env.SITE_NAME : 'Paper'
-  return { props: { siteName } }
+  return { props: { siteName, posts: getAllArticles() } }
 }
