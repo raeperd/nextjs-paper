9d961ce11dc8592ff93646be125d24133bf7dd1d
raeperd <raeperd117@gmail.com>
2022-01-09 10:03:17 +0900

    Fix title in Index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
-export default function Index() {
+import Head from 'next/head';
+
+export default function Index({ siteName }: {siteName: string}) {
   return (
-    <>
-    </>
+    <Head>
+      <title>{siteName}</title>
+    </Head>
   )
+}
+
+export async function getStaticProps() {
+  const siteName = process.env.SITE_NAME ? process.env.SITE_NAME : 'Paper'
+  return { props: { siteName } }
 }
