ca1c8b8a79bf61429a6df29f8077cb837ad5d0bb
raeperd <raeperd@users.noreply.github.com>
2022-01-13 12:56:44 +0000

    deploy: c9eb29ecfa95df415a5a0ef4e738684c4061b8e1

---
+++ _next/static/b9zFDvMvD2fF7daYtjaEK/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fpage\u002F[pageNumber]","\u002Ftags\u002F[tag]","\u002Ftags\u002F[tag]\u002Fpage\u002F[pageNumber]","\u002Fpost\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
